48395b4db47a5e4f07ff28c8f06e09c306f3f90b
EnOte <164638951+EnOtese@users.noreply.github.com>
2024-09-19 21:05:53 +0400

    Add files via upload

---
+++ module7_5.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+
+for root, dirs, files in os.walk('.'):
+    for file in files:
+        filepath = os.path.join(file)
+        filetime = os.path.getmtime(file)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(file)
+        parentdir = os.path.dirname(file)
+
+    print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parentdir}')
