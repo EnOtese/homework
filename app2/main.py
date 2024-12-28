8d7899b484f534dd78597d480a0c9f5561506604
EnOte <164638951+EnOtese@users.noreply.github.com>
2024-12-28 20:04:30 +0400

    Add files via upload

---
+++ app2/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app2.routers import task, user
+
+app = FastAPI()
+
+
+# python -m uvicorn app2.main:app2
+
+@app.get('/')
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(task.router)
+app.include_router(user.router)
